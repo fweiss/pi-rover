550c616b63b8493cf3cf9a321dc671f27b5ac0fe
Frank E Weiss <feweiss@gmail.com>
2020-02-11 13:24:25 -0800

    Refactor: extract battery service

--- src/main/robby/main.py
+++ src/main/robby/main.py
@@ -21,6 +21,7 @@ from exceptions import NotSupportedException, InvalidValueLengthException, NotPe
 from advertisement import Advertisement
 from service import Service
 from characteristic import Characteristic
+from battery_service import BatteryService
 
 mainloop = None
 
@@ -244,75 +245,6 @@ class HeartRateControlPointChrc(Characteristic):
 
         print('Energy Expended field reset!')
         self.service.energy_expended = 0
-
-
-class BatteryService(Service):
-    """
-    Fake Battery service that emulates a draining battery.
-
-    """
-    BATTERY_UUID = '180f'
-
-    def __init__(self, bus, index):
-        Service.__init__(self, bus, index, self.BATTERY_UUID, True)
-        self.add_characteristic(BatteryLevelCharacteristic(bus, 0, self))
-
-
-class BatteryLevelCharacteristic(Characteristic):
-    """
-    Fake Battery Level characteristic. The battery level is drained by 2 points
-    every 5 seconds.
-
-    """
-    BATTERY_LVL_UUID = '2a19'
-
-    def __init__(self, bus, index, service):
-        Characteristic.__init__(
-            self, bus, index,
-            self.BATTERY_LVL_UUID,
-            ['read', 'notify'],
-            service)
-        self.notifying = False
-        self.battery_lvl = 100
-        GObject.timeout_add(5000, self.drain_battery)
-
-    def notify_battery_level(self):
-        if not self.notifying:
-            return
-        self.PropertiesChanged(
-            GATT_CHRC_IFACE,
-            { 'Value': [dbus.Byte(self.battery_lvl)] }, [])
-
-    def drain_battery(self):
-        if not self.notifying:
-            return True
-        if self.battery_lvl > 0:
-            self.battery_lvl -= 2
-            if self.battery_lvl < 0:
-                self.battery_lvl = 0
-        print('Battery Level drained: ' + repr(self.battery_lvl))
-        self.notify_battery_level()
-        return True
-
-    def ReadValue(self, options):
-        print('Battery Level read: ' + repr(self.battery_lvl))
-        return [dbus.Byte(self.battery_lvl)]
-
-    def StartNotify(self):
-        if self.notifying:
-            print('Already notifying, nothing to do')
-            return
-
-        self.notifying = True
-        self.notify_battery_level()
-
-    def StopNotify(self):
-        if not self.notifying:
-            print('Not notifying, nothing to do')
-            return
-
-        self.notifying = False
-
 
 class TestService(Service):
     """
