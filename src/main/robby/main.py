49ae13bb78ac7f0b0300ae289c17a78ad4a12ca0
Frank E Weiss <feweiss@gmail.com>
2020-02-11 15:19:37 -0800

    Refactor: renomve dead code

--- src/main/robby/main.py
+++ src/main/robby/main.py
@@ -8,25 +8,16 @@ import dbus.exceptions
 import dbus.mainloop.glib
 import dbus.service
 
-import array
 try:
     from gi.repository import GObject
 except ImportError:
     import gobject as GObject
 import sys
 
-from random import randint
-
-from exceptions import NotSupportedException, InvalidValueLengthException, NotPermittedException
 from advertisement import Advertisement
-from service import Service
-from characteristic import Characteristic
 from battery_service import BatteryService
 from test_service import TestService
 from heart_rate_service import HeartRateService
-
-# only for TestService
-from descriptor import Descriptor
 
 mainloop = None
 
@@ -35,28 +26,10 @@ GATT_MANAGER_IFACE = 'org.bluez.GattManager1'
 DBUS_OM_IFACE =      'org.freedesktop.DBus.ObjectManager'
 DBUS_PROP_IFACE =    'org.freedesktop.DBus.Properties'
 
-# GATT_SERVICE_IFACE = 'org.bluez.GattService1'
 GATT_CHRC_IFACE =    'org.bluez.GattCharacteristic1'
 GATT_DESC_IFACE =    'org.bluez.GattDescriptor1'
 
-# LE_ADVERTISEMENT_IFACE = 'org.bluez.LEAdvertisement1'
 LE_ADVERTISING_MANAGER_IFACE = 'org.bluez.LEAdvertisingManager1'
-
-# class InvalidArgsException(dbus.exceptions.DBusException):
-#     _dbus_error_name = 'org.freedesktop.DBus.Error.InvalidArgs'
-#
-# class NotSupportedException(dbus.exceptions.DBusException):
-#     _dbus_error_name = 'org.bluez.Error.NotSupported'
-#
-# class NotPermittedException(dbus.exceptions.DBusException):
-#     _dbus_error_name = 'org.bluez.Error.NotPermitted'
-#
-# class InvalidValueLengthException(dbus.exceptions.DBusException):
-#     _dbus_error_name = 'org.bluez.Error.InvalidValueLength'
-#
-# class FailedException(dbus.exceptions.DBusException):
-#     _dbus_error_name = 'org.bluez.Error.Failed'
-#
 
 class Application(dbus.service.Object):
     """
