d1e66792de7b710c18218cf8f104f80ca16b280b
Frank E Weiss <feweiss@gmail.com>
2020-02-11 14:57:13 -0800

    Refactor: extract test service

--- src/main/robby/descriptor.py
+++ src/main/robby/descriptor.py
@@ -1,6 +1,8 @@
 import dbus
 
-from exceptions import InvalidArgsException, NotSupportedException
+import array
+
+from exceptions import InvalidArgsException, NotSupportedException, NotPermittedException
 
 GATT_DESC_IFACE =    'org.bluez.GattDescriptor1'
 DBUS_PROP_IFACE =    'org.freedesktop.DBus.Properties'
@@ -50,3 +52,27 @@ class Descriptor(dbus.service.Object):
         print('Default WriteValue called, returning error')
         raise NotSupportedException()
 
+class CharacteristicUserDescriptionDescriptor(Descriptor):
+    """
+    Writable CUD descriptor.
+
+    """
+    CUD_UUID = '2901'
+
+    def __init__(self, bus, index, characteristic):
+        self.writable = 'writable-auxiliaries' in characteristic.flags
+        self.value = array.array('B', b'This is a characteristic for testing')
+        self.value = self.value.tolist()
+        Descriptor.__init__(
+            self, bus, index,
+            self.CUD_UUID,
+            ['read', 'write'],
+            characteristic)
+
+    def ReadValue(self, options):
+        return self.value
+
+    def WriteValue(self, value, options):
+        if not self.writable:
+            raise NotPermittedException()
+        self.value = value
