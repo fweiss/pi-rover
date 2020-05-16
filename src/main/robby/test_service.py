dbd27d427de08012c93772104bb9dbf3f4bd89f4
Frank E Weiss <feweiss@gmail.com>
2020-05-16 15:23:48 -0700

    Comment out uneeded service from example code

--- src/main/robby/test_service.py
+++ src/main/robby/test_service.py
@@ -34,8 +34,8 @@ class TestService(Service):
     def __init__(self, bus, index):
         Service.__init__(self, bus, index, self.TEST_SVC_UUID, True)
         self.add_characteristic(TestCharacteristic(bus, 0, self))
-        self.add_characteristic(TestEncryptCharacteristic(bus, 1, self))
-        self.add_characteristic(TestSecureCharacteristic(bus, 2, self))
+        # self.add_characteristic(TestEncryptCharacteristic(bus, 1, self))
+        # self.add_characteristic(TestSecureCharacteristic(bus, 2, self))
 
 class TestCharacteristic(Characteristic):
     """
